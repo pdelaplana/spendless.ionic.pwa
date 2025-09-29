23e4124f1caf4c92dad2001eabf9d7350d16a461
pdelaplana <patrick.delaplana@gmail.com>
2025-09-30 09:43:26 +1000

    disable dark mode (for now)

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,10 @@ import '@ionic/react/css/display.css';
  * https://ionicframework.com/docs/theming/dark-mode
  */
 
+/* Dark mode disabled */
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+/* import '@ionic/react/css/palettes/dark.system.css'; */
 
 /* Theme variables */
 import './theme/variables.css';
